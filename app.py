de74e4bb9d17c2b83afefd965f7675b85d9df040
mohammedsavad <89774398+mohammedsavad@users.noreply.github.com>
2022-06-02 09:52:14 +0530

    count added

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import random
 
-while(True):
-    rabbit = False
-    tortoise = False 
+def number_generater():
     system_generated = ""
     for i in range(4):system_generated += str(random.randint(0,9))
-    user_input =  input("Enter a four digit Positive number : ")
+    return system_generated
+
+system_generated = number_generater()
+
+while(True):
+    rabbit = 0
+    tortoise = 0
+    user_input =  input("Enter a four digit number : ")
     if user_input[0] == '-' or user_input[0] == '+':user_input = user_input[1:]
     try:
         temp = int(user_input)
-        if len(user_input) != 4 or temp < 0:
-            print("Enter a valid number")
+        if len(user_input) != 4:
+            print("Number must be 4 digit Postive number")
             continue
     except:
         print("Enter a valid number")
@@ -18,19 +23,19 @@ while(True):
     print("system Guess : ",system_generated)
     if system_generated == user_input :
         print("WINNER")
+        system_generated = number_generater()
     else:
         for i in range(4):
             if user_input[i] == system_generated[i]:
-                rabbit = True
+                rabbit += 1
             elif user_input[i] in system_generated:
-                tortoise = True
-    if rabbit:
-        print("You got rabbit")
-    elif tortoise:
-        print("You got tortoise")
+                tortoise +=1
+    if rabbit > 0:
+        print(f"You got rabbit : {rabbit} times "  )
+    if tortoise > 0:
+        print(f"You got tortoise : {tortoise} times ")
 
     button = input("Do you want To Continue? \n press [yes] : ")
     if button.lower() != 'yes':
         break
-
 
