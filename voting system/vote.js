2f7d8f4fccbd19c5567fcbc90b7fd5af46aba8a5
mohammedsavad <89774398+mohammedsavad@users.noreply.github.com>
2022-06-21 22:15:32 +0530

    Update vote.js

--- voting system/vote.js
+++ voting system/vote.js
@@ -1,4 +1,6 @@
+let candidate_id = 1001
 $(document).ready(function () {
+    $('#id').val(candidate_id)
     $('#vvote').hide();
     $('#adddt').hide();
     $('#result').hide();
@@ -14,19 +16,25 @@ $("form").submit(function(e) {
     var name=$("input[name='name']").val();
     var id=$("input[name='id']").val();
   
-    candidates[$('#name').val()] = 0
+    candidates[$('#id').val()] = 0
      
      $(".data-table #first").append("<tr data-name='"+name+"' data-id='"+id+"'><td>"+name+"</td><td>"+id+"</td><td><button class='btn btn-danger btn-lg btn-delete mr-3' type='button'>Delete</button><button class='btn btn-info btn-lg btn-edit' type='button'>Edit</button></tr>");
-     $("#adddt #cand").append("<tr data-name='"+name+"' data-id='"+id+"'><td>"+name+"</td><td>"+id+"</td><td id='votng'><button class='btn btn-danger btn-lg btn-vote '  type='button'>vote</button></tr>");
+     $("#adddt #cand").append("<tr data-name='"+name+"' data-id='"+id+"'><td>"+name+"</td><td>"+id+"</td><td id='votng'><button class='btn btn-danger btn-lg btn-vote ' id="+id+"  type='button'>vote</button></tr>");
      //$("#vdt").append("<tr data-name='"+name+"' data-id='"+id+"'><td>"+name+"</td><td>"+id+"</td><td id='count'></tr>");
-
+    candidate_id+=1;
+    
+    $('#id').val(candidate_id)
      $("input[name='']").val("");
      $('#vvote').show();
      $('#ad').show();
   });
 
   $('body').on('click','.btn-delete',function() {
+    let get_id = $(this).parents('tr').data('id')
+    $('#'+get_id).parents('tr').remove()
     $(this).parents('tr').remove();
+
+
   });
 
   $('body').on('click','.btn-edit',function() {
@@ -68,9 +76,18 @@ $("form").submit(function(e) {
 
   });
   $('body').on('click','#result',function() {
-    for (let key in candidates) { 
-        $('#retab').html($('#retab').html()+` <tr><td>${key}</td><td>${candidates[key]}</td></tr>`)
+    let sortable = [];
+    for (var key in candidates) {
+        sortable.push([key, candidates[key]]);
     }
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    sortable.forEach(element => {
+        temp_name = $('#'+element[0]).parents('tr').data('name')
+        $('#retab').html($('#retab').html()+` <tr><td>${temp_name}</td><td>${element[1]}</td></tr>`)
+    });
+        
     $('.container').hide();
     $('#adddt').hide();
     $('#result').hide();
@@ -78,9 +95,6 @@ $("form").submit(function(e) {
   });
    
 $('body').on('click','#votng',function() {
-    var name=$(this).parents('tr').data('name')
-    console.log(name)
-    candidates[name]+=1;
-    console.log(candidates[name])
+    var id=$(this).parents('tr').data('id')
+    candidates[id]+=1;
 });
-
